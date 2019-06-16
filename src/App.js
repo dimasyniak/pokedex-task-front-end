7e2d182ac64f94945875481b0a7528aee02ea025
Dima Syniak <syniakdima@gmail.com>
2019-06-16 16:57:19 +0300

    Save worked project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { observable } from "mobx";
+import { observer } from "mobx-react";
+
+@observer
+class App extends React.Component {
+  @observable count = 0;
+
+  incHandler = () => {
+    this.count++;
+  };
+
+  decHandler = () => {
+    this.count--;
+  };
+  render() {
+    const [counts, setCount] = useState(2000);
+    return (
+      <div>
+        <button onClick={this.incHandler}>+</button>
+        <button onClick={this.decHandler}>-</button>
+        <button onClick={() => setCount(5000)}>-</button>
+        {this.count}
+        {counts}
+      </div>
+    );
+  }
 }
 
 export default App;
