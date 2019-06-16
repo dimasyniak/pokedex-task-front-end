c301106c38b702c0938d7a131a2f4a225418ae59
Dima Syniak <syniakdima@gmail.com>
2019-06-16 17:03:12 +0300

    Save worked project 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { observable } from "mobx";
 import { observer } from "mobx-react";
@@ -16,12 +16,21 @@ class App extends React.Component {
   };
   render() {
     const [counts, setCount] = useState(2000);
+
+    useEffect(() => {
+      // Обновляем заголовок документа, используя API браузера
+      document.title = `Вы нажали ${this.count} раз`;
+    });
+
     return (
       <div>
         <button onClick={this.incHandler}>+</button>
         <button onClick={this.decHandler}>-</button>
-        <button onClick={() => setCount(5000)}>-</button>
+        <hr />
+        <button onClick={() => setCount(this.count)}>ok</button>
+        <hr />
         {this.count}
+        <hr />
         {counts}
       </div>
     );
